a7bee67359710568e50022cebfe0db36ec2f0989
NAKAJIMA Yusaku <yupong7@gmail.com>
2018-12-15 00:36:13 +0900

    add match().

--- parse.c
+++ parse.c
@@ -1,9 +1,12 @@
 #include <ctype.h>
 #include <stdio.h>
 #include "main.h"
-
+#include "util.h"
 
 /*
+ * 最適化: rest() 内での尾部再帰を繰り返しで置き換えたら、
+ * rest() の呼び出しは expr からのみとなるため、 expr() 内に
+ * rest() の処理を統合することができる。
  * expr: term rest
  */
 void expr() {
@@ -11,6 +14,7 @@ void expr() {
 }
 
 /*
+ * 最適化: 尾部再帰を繰り返しで置き換えられる。
  * rest: '+' term { print('+') } rest # (1)
  * rest: '-' term { print('-') } rest # (2)
  * rest: ε                            # (3)
@@ -31,5 +35,12 @@ void rest() {
 void term() {
   if (isdigit(lookahead)) {
     putchar(lookahead); match(lookahead);
-  } else error();
+  } else error("syntax error");
+}
+
+void match(const int c) {
+  if(lookahead == c)
+    lookahead = getchar();
+  else
+    error("syntax error");
 }
