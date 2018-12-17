b5fe9d50429a0445af8c553c62f535e931d94d3d
NAKAJIMA Yusaku <yupong7@gmail.com>
2018-12-18 07:49:37 +0900

    add Vector, Map

--- util.h
+++ util.h
@@ -1,4 +1,28 @@
 #ifndef UTIL_H
 #define UTIL_H
+// util
+typedef struct {
+  void **data;
+  int capacity;
+  int len;
+} Vector;
+
+Vector *new_vector();
+void vec_push(Vector *, void *);
+
+typedef struct {
+  Vector *keys;
+  Vector *vals;
+} Map;
+
+Map *new_map();
+void map_put(Map *, char *, void *);
+void *map_get(Map *, char *);
+
+int *intdup(int);
+
 _Noreturn void error(char *, ...);
+
+// util_test
+void run_utiltest();
 #endif
